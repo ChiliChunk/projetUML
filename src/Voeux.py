18e52bbe1bafbdd3963d3d0004e022af9abf4f63
Nicolas BRIET <fussure@gmail.com>
2019-01-07 17:52:13 +0100

    end of the day

--- src/Voeux.py
+++ src/Voeux.py
@@ -6,4 +6,11 @@ class Voeux:
         self.typeBien = typeBien
         self.prixSouhaite = prixSouhaite
         self.localisation = localisation
-        if typeBien == BienImmobilier.TypesBien.
+        
+        if typeBien == BienImmobilier.TypesBien.APPARTEMENT:
+            self.nombrePiece = nombrePiece
+        if typeBien == BienImmobilier.TypesBien.TERRAIN:
+            self.surfaceAuSol = surfaceAuSol
+        if typeBien == BienImmobilier.TypesBien.MAISON:
+            self.nombrePiece = nombrePiece
+            self.surfaceAuSol = surfaceAuSol
