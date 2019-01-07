ebbc752ff65cebc06f04d5890923788587fff8f5
etu <bourgon.nils@gmail.com>
2019-01-07 17:23:36 +0100

    Ajout enum biens

--- src/BienImmobilier.py
+++ src/BienImmobilier.py
@@ -1,4 +1,6 @@
+from enum import Enum
 class BienImmobilier :
+    TypesBien = Enum("Maison","Terrain","Appartement")
 
     def __init__(self, prix,dateVente,adresse,orientation,dateDispo):
         self.prix = prix
@@ -6,4 +8,6 @@ class BienImmobilier :
         self.adresse = adresse
         self.orientation = orientation
         self.dateDispo = dateDispo
+
+
 
