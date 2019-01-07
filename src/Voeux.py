4078292b8c802f86c79c7f4764a467f876897cf0
Nicolas BRIET <fussure@gmail.com>
2019-01-07 17:35:35 +0100

    backup

--- src/Voeux.py
+++ src/Voeux.py
@@ -1,6 +1,9 @@
+from src.BienImmobilier import BienImmobilier
+
 class Voeux:
 
     def __init__(self , typeBien , prixSouhaite , localisation , nombrePiece=None , surfaceAuSol=None):
         self.typeBien = typeBien
         self.prixSouhaite = prixSouhaite
         self.localisation = localisation
+        if typeBien == BienImmobilier.TypesBien.
