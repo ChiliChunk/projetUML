bbf2974447404c520f519b8cff768cfe62e083a2
etu <bourgon.nils@gmail.com>
2019-01-08 16:01:05 +0100

    End of the day ...

--- src/Agence.py
+++ src/Agence.py
@@ -11,12 +11,12 @@ class Agence:
         self.rdvs.append(Rdv(bien=bien,vendeur=bien.vendeur, type=TypesRdv.MANDAT,date=date))
 
     def prendreRdvVente(self, bien, date, acheteur):
+        ...
         #self.rdvs.append(Rdv(bien=bien,vendeur=bien.vendeur, type=TypesRdv.VENTE,date=date))
 
     def prendreRdvVisite(self, bien,date, visiteur):
+        ...
         #self.rdvs.append(Rdv(bien=bien,vendeur=bien.vendeur, type=TypesRdv.VISITE,date=date))
-    
+
     def __str__(self):
         return (f"rdvs : {''.join(str(e) for e in self.rdvs)} biens : {''.join(str(e) for e in self.biensImmobiliers)} annonces : {''.join(str(e) for e in self.annonces)} personnes : {''.join(str(e) for e in self.personnes)} NOM : {self.nom}")
-    
-    
