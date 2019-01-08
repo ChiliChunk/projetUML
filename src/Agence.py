f49041e2cd8c61612f475b2fc8e5b9199dc63643
Nicolas BRIET <fussure@gmail.com>
2019-01-08 15:52:20 +0100

    add main

--- src/Agence.py
+++ src/Agence.py
@@ -1,7 +1,12 @@
 class Agence:
-    def __init__(self , nom='Agence Par defaut'):
+    def __init__(self , nom="Agence Par defaut"):
         self.rdvs = []
         self.biensImmobiliers = []
         self.annonces = []
         self.personnes = []
         self.nom = nom
+
+    def __str__(self):
+        return (f"rdvs : {''.join(str(e) for e in self.rdvs)} biens : {''.join(str(e) for e in self.biensImmobiliers)} annonces : {''.join(str(e) for e in self.annonces)} personnes : {''.join(str(e) for e in self.personnes)} NOM : {self.nom}")
+    
+    
--- src/Personne.py
+++ src/Personne.py
@@ -1,4 +1,7 @@
 class Personne:
 
-    def __init__(self , unVoeux = None):
+    def __init__(self ,unVoeux = None):
         self.voeux = unVoeux
+
+    def inscrire(self, agence):
+        agence.personnes.append(self)
