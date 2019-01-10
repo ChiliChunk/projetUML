5e99d6db6c895cf5be27c64d8c2fc29af1455e8c
Nicolas BRIET <fussure@gmail.com>
2019-01-10 17:05:36 +0100

    ajout voeux WIP

---
+++ src/IHM/DialAchatBien.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+master = Tk()
+
+
+def renderFields(typeSelected):
+    Label(master, text="Prix souhaité").grid(row=2)
+    entryPrix = Entry(master)
+    entryPrix.grid(row=2, column=1)
+    Label(master, text="Localisation").grid(row=3)
+    entryLocalisation = Entry(master)
+    entryLocalisation.grid(row=3, column=1)
+    if typeSelected == "Appartement":
+        labNbPiece = Label(master, text="Nombre de piece").grid(row=4)
+        entryNbPice = Entry(master)
+        entryNbPice.grid(row=4, column=1)
+
+    if typeSelected == "Terrain":
+        labSurface = Label(master, text="Surface (m²)").grid(row=4)
+        entrySurface = Entry(master)
+        entrySurface.grid(row=4, column=1)
+
+    if typeSelected == "Maison":
+        labNbPiece =  Label(master, text="Nombre de piece").grid(row=4)
+        entrySurface = Label(master, text="Surface (m²)").grid(row=5)
+        entryNbPice = Entry(master)
+        entrySurface = Entry(master)
+        entryNbPice.grid(row=4, column=1)
+        entrySurface.grid(row=5, column=1)
+
+
+def changeHandler(event):
+    renderFields(event)
+
+def renderDropdownMenu(master):
+    variable = StringVar(master)
+    variable.set("Appartement")  # default value
+    OptionMenu(master, variable, "Appartement", "Terrain", "Maison", command=changeHandler).grid(row=1, column="1")
+
+
+def renderDialAchatBien():
+
+    Label(master, text="Entrer un voeux d'achat").grid(row=0)
+    renderDropdownMenu(master)
+    mainloop( )
+
+renderDialAchatBien()
